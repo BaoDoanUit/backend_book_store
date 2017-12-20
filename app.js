a8303ff893774b5a8083b65d0249fb096e0d5ba1
Nhan <nguyenthanhnhan9516@gmail.com>
2017-12-20 17:54:56 +0200

    Fixed errors

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-var app = require("express")();
+// import libs
+var express = require('express');
+var app = express();
 var bodyParser = require("body-parser");
-var bookController = require("./controller/book");
-var data = require("./models/book");
-app.use(bodyParser.urlencoded());
-app.route('/books')
-    .get(bookController.getAll)
-    .post(bookController.create)
-    .put()
-    .delete();
-app.route('/books/:id')
-    .get(bookController.getOne)
-    .post()
-    .put(bookController.update)
-    .delete(bookController.delete);
 
-app.listen(3333)
+// controllers
+var bookController = require("./controller/book");
+
+// define middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// define routing 
+app.route('/books')
+  .get(bookController.getAll)
+  .post(bookController.create)
+  .put()
+  .delete();
+
+app.route('/books/:id')
+  .get(bookController.getOne)
+  .post()
+  .put(bookController.update)
+  .delete(bookController.delete);
+
+var port = process.env.PORT || 3333; 
+app.listen(port, function() {
+  console.log("Server is running on port: " + port)
+})
--- controller/book.js
+++ controller/book.js
@@ -1,25 +1,29 @@
+var data = require("../models/book");
+
 var books = {
     getAll: function(req, res) {
         var allbooks = data;
         res.json(allbooks);
     },
-getOne: function(req, res) {
-        var id = req.params.id;
-        var books = data[0];
-        res.json(books);
+    getOne: function(req, res) {
+        data.filter(function(book) {
+            if (book.id == req.params.id) {
+                res.json(book);
+            }
+        });
     },
-create: function(req, res) {
+    create: function(req, res) {
         var newbook = req.body;
         data.push(newbook);
         res.json(newbook);
     },
-update: function(req, res) {
+    update: function(req, res) {
         var updatebook = req.body;
         var id = req.params.id;
         data[id] = updatebook
         res.json(updatebook);
     },
-delete: function(req, res) {
+    delete: function(req, res) {
         var id = req.params.id;
         data.splice(id, 1)
         res.json(true);
