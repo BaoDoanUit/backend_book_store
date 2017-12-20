f5eacff9dac9c5199ecd0e19f16f9b884d0465a6
BaoDoan <baodoanuit@gmail.com>
2017-12-20 22:14:13 +0700

    update

---
+++ controller/book.js
@@ -0,0 +1,28 @@
+var books = {
+    getAll: function(req, res) {
+        var allbooks = data;
+        res.json(allbooks);
+    },
+getOne: function(req, res) {
+        var id = req.params.id;
+        var books = data[0];
+        res.json(books);
+    },
+create: function(req, res) {
+        var newbook = req.body;
+        data.push(newbook);
+        res.json(newbook);
+    },
+update: function(req, res) {
+        var updatebook = req.body;
+        var id = req.params.id;
+        data[id] = updatebook
+        res.json(updatebook);
+    },
+delete: function(req, res) {
+        var id = req.params.id;
+        data.splice(id, 1)
+        res.json(true);
+    }
+};
+module.exports = books;
